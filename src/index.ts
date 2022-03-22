2a7c38b70e2a0a2b070f11d06b707d1f781cc894
Shawn Mitchell <shawn.mitchell>
2022-03-22 11:01:08 -0400

    add verify proofs

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
-import { MerkleTree } from 'merkletreejs';
+import { MerkleTree,  } from 'merkletreejs';
 import { ethers, providers, Signer, utils } from 'ethers';
 import * as MyRegistrar from './contracts/MyRegistrar.json';
 import * as crypto from 'crypto';
 
 function sha256(data: string) {
   return crypto.createHash('sha256').update(data).digest()
+}
+
+interface Proof {
+  position: 'left' | 'right';
+  data: any;
+}
+
+interface PortableProof {
+  index: number;
+  key: string;
+  value: string;
+  proof: Proof[];
 }
 
 const CONTRACT_ADDRESS = '0x5b7D656f9D21d9FC727d8C52eb939661aF28D680';
@@ -21,6 +33,16 @@ export const generateMerkleTree = (input: string[]) => {
   const tree = new MerkleTree(leaves, sha256);
   
   return tree;
+}
+
+export const verifyProofs = async (proofs: PortableProof[], root: string) => {
+
+  try {
+    return proofs.every((proof) => MerkleTree.verify(proof.proof, `${proof.key}:${proof.value}`, root));
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
 }
 
 export const doMerkleVerify = (tree: MerkleTree, toTest: string, root: string) => {
